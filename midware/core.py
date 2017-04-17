b165cfe00eb0b6dd4cd579e068db0dc9d30ee35d
Ivan Dmitrievsky <ivan.dmitrievsky@gmail.com>
2017-04-17 22:12:12 +0300

    Write documentation for the core

--- midware/core.py
+++ midware/core.py
@@ -1,16 +1,40 @@
 # -*- coding: utf-8 -*-
+"""
+Midware is a general-purpose middleware library.
+
+The purpose of middleware is to wrap user-defined handlers.
+All middleware consists of two sections: `before` and `after`.
+There is a special function that controls middleware execution order.
+It threads a dictionary called `env` through the `before` sections
+then calls the handler and then threads the handler's result
+through `after` sections.
+"""
 
 from contextlib import contextmanager
 import inspect
 
+from .dict import assoc_in
+
+"""
+A global flag that controls printing debug information. 
+"""
 _verbose_mode = False
 
 
 def identity(x):
+    """
+    A named identity function is nicer than `lambda x: x`.
+    """
     return x
 
 
 def compose(*funcs):
+    """
+    Returns a function that applies other functions in sequence.  
+    `compose(f, g, h)(x, y)` is the same as `f(g(h(x, y)))`.
+     
+    If no arguments are provided, the identity function is returned.
+    """
     if not funcs:
         return identity
 
@@ -26,26 +50,47 @@ def compose(*funcs):
     return wrapper
 
 
-def print_inwards(middleware_name, hidden_mw=False):
+def _print_inwards(middleware_name, hidden_mw=False):
+    """
+    Print a `middleware_name` with an inwards arrow
+    if `_verbose_mode` is on and if middleware is not hidden.
+    """
     if _verbose_mode and not hidden_mw:
         print('{}--->'.format(middleware_name))
 
 
-def print_outwards(middleware_name, hidden_mw=False):
+def _print_outwards(middleware_name, hidden_mw=False):
+    """
+    Print a `middleware_name` with an outwards arrow
+    if `_verbose_mode` is on and if middleware is not hidden.
+    """
     if _verbose_mode and not hidden_mw:
         print('<---{}'.format(middleware_name))
 
 
 def wrapper_from_contextmanager(cm, handler, ks=None):
+    """
+    Context managers and middleware wrappers have similar semantics,
+    which makes converting context manager to a wrapper really easy.
+    
+    This function will be called from the body of a named middleware,
+    so capturing this name needs to be done here. The wrapper itself will
+    be called from elsewhere.
+    """
     middleware_name = inspect.stack()[1][3]
 
     def wrapper(env):
-        print_inwards(middleware_name, hasattr(cm, 'hidden_mw'))
+        """
+        Context manager is passed `env` as its only argument.
+        The result of entering `cm` can be saved in `env`
+        if the `ks` sequence of keys is specified.
+        """
+        _print_inwards(middleware_name, hasattr(cm, 'hidden_mw'))
         with cm(env) as v:
             if ks:
                 assoc_in(env, ks, v)
             handled_env = handler(env)
-        print_outwards(middleware_name, hasattr(cm, 'hidden_mw'))
+        _print_outwards(middleware_name, hasattr(cm, 'hidden_mw'))
 
         return handled_env
 
@@ -53,15 +98,23 @@ def wrapper_from_contextmanager(cm, handler, ks=None):
 
 
 def wrapper_from_generator(g, handler, ks=None):
+    """
+    Generators with one `yield` statement are similar to
+    context managers.
+
+    Even though the code is the same just for `contextmanager(g)`
+    instead of `cm`, it's not possible to reuse `wrapper_from_contextmanager`
+    because `middleware_name` will be set to `wrapper_from_generator` (the caller).
+    """
     middleware_name = inspect.stack()[1][3]
 
     def wrapper(env):
-        print_inwards(middleware_name, hasattr(g, 'hidden_mw'))
+        _print_inwards(middleware_name, hasattr(g, 'hidden_mw'))
         with contextmanager(g)(env) as v:
             if ks:
                 assoc_in(env, ks, v)
             handled_env = handler(env)
-        print_outwards(middleware_name, hasattr(g, 'hidden_mw'))
+        _print_outwards(middleware_name, hasattr(g, 'hidden_mw'))
 
         return handled_env
 
@@ -69,57 +122,46 @@ def wrapper_from_generator(g, handler, ks=None):
 
 
 def _verbose(_):
+    """
+    This wrapper doesn't change the `env` passed to it. In its
+    `before` section it turns `_verbose_mode` on and turns it off
+    in the `after` section.
+    """
     global _verbose_mode
     _verbose_mode = True
     yield
     _verbose_mode = False
 
 
-def verbose_wrapper(handler):
+def wrap_verbose(handler):
+    """
+    This middleware gets silently inserted into the collection
+    of user-specified middleware, so it shouldn't be visible in
+    debug output. To hide it `hidden_mw` attribute is set
+    on the generator.
+    """
     _verbose.hidden_mw = True
     return wrapper_from_generator(_verbose, handler)
 
 
-def through(env, handler, *wrappers, verbose=False):
-    wrappers_ = list(wrappers)
+def pipe_through(env, handler, *middleware, verbose=False):
+    """
+    This function kicks of the execution. The `env` is piped
+    through `before` sections of `middleware` left to right
+    and after the `handler` is executed the `env` is piped
+    through `after` sections right to left.
+    
+    Setting `verbose` true inserts a hidden `wrap_verbose`
+    middleware to the beginning.
+    """
+    middleware_ = list(middleware)
     if verbose:
-        wrappers_.insert(0, verbose_wrapper)
-    return compose(*reversed(wrappers_))(handler)(env)
+        middleware_.insert(0, wrap_verbose)
+    return compose(*reversed(middleware_))(handler)(env)
 
 
-def clean_through(handler, *wrappers, verbose=False):
-    return through({}, handler, *wrappers, verbose=verbose)
-
-
-def inc(x):
-    return x + 1
-
-
-def dec(x):
-    return x - 1
-
-
-def no_op(_):
-    yield
-
-
-def wrap_no_op(handler):
-    return wrapper_from_generator(no_op, handler)
-
-
-def inc_n(env):
-    yield
-    update_in(env, ['n'], inc)
-
-
-def wrap_inc_n(handler):
-    return wrapper_from_generator(inc_n, handler)
-
-
-def req(env):
-    print('CTR')
-    assoc_in(env, ['n'], 0)
-    return env
-
-
-print(clean_through(req, wrap_no_op, wrap_inc_n))
+def exec_seq(handler, *middleware, verbose=False):
+    """
+    The same as `pipe_through`, but starts with an empty `env`.
+    """
+    return pipe_through({}, handler, *middleware, verbose=verbose)
