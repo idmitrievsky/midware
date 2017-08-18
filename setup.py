c561dc5929e7453beb89b6016ea5ae43c6e3d7a5
Ivan Dmitrievsky <ivan.dmitrievsky@gmail.com>
2017-08-18 18:33:38 +0300

    Include LICENSE into distribution

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('LICENSE') as f:
 
 setup(
     name='midware',
-    version='0.1.0',
+    version='0.1.4',
     description='A simple general-purpose middleware library for Python',
     long_description=readme,
     author='Ivan Dmitrievsky',
